42f86e41209fe22f7c18d5db6c5b0aa5a4496f35
Pratham700 <prathamsinghal.singhal@gmail.com>
2020-11-17 12:38:37 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,20 @@ import '../styles/App.css';
 
 class App extends Component {
     render() {
-
+    const relList = ["Father","Mother","Brother","Sister"]
         return(
             <div id="main">
                {/* Do not remove the main div */}
+               <ol key = "relativesList">
+                   {
+                relList.map( (rel,idx) => (
+                    <li key = {"relListItem" + (idx+1)}>{rel}</li>
+                ) )}
+               </ol>
+              
             </div>
-        )
-    }
+        );
+    };
 }
 
 
